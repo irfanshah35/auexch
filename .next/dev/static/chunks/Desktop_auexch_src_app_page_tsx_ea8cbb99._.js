1a85c267e18d3e1623c5767f3bb054fb718ae0b9
irfanshah35 <irfanshah@universalplus.technology>
2026-02-15 12:02:32 +0500

    swiepradd

--- .next/dev/static/chunks/Desktop_auexch_src_app_page_tsx_ea8cbb99._.js
+++ .next/dev/static/chunks/Desktop_auexch_src_app_page_tsx_ea8cbb99._.js
@@ -1,7 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/Desktop_auexch_src_961fd94e._.js"
+  "static/chunks/72895_71222b9a._.js",
+  "static/chunks/Desktop_auexch_src_961fd94e._.js",
+  "static/chunks/72895_swiper_d930a848._.css"
 ],
     source: "dynamic"
 });
--- src/components/home-slider/page.tsx
+++ src/components/home-slider/page.tsx
@@ -1,11 +1,97 @@
 "use client";
+import { Swiper as SwiperComponent, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
- 
+const staticSliders = [
+  { id: "static-1", url: "https://static.assetsdelivery.net/marketing-posters/DOLLAR365COM/1758618782551572.jpeg" },
+  { id: "static-2", url: "https://static.assetsdelivery.net/marketing-posters/DOLLAR365COM/1770989232480743.jpeg" },
+  { id: "static-3", url: "https://static.assetsdelivery.net/marketing-posters/DOLLAR365COM/1770989245911070.jpeg" },
+  { id: "static-4", url: "https://static.assetsdelivery.net/marketing-posters/DOLLAR365COM/1758881377636214.jpeg" },
+  { id: "static-5", url: "https://static.assetsdelivery.net/marketing-posters/DOLLAR365COM/1759080900908840.jpeg" },
+  { id: "static-6", url: "https://static.assetsdelivery.net/marketing-posters/DOLLAR365COM/1770989265863471.jpeg" },
+];
+
+// 👇 Wapas purani positions lagadi hain (Top-Left for dots)
+const sliderWrapperStyles = `w-full relative rounded-xl overflow-hidden group 
+  [&_.custom-pagination]:!absolute [&_.custom-pagination]:!top-4 [&_.custom-pagination]:!bottom-auto [&_.custom-pagination]:!left-4 [&_.custom-pagination]:!w-auto [&_.custom-pagination]:z-50 [&_.custom-pagination]:flex [&_.custom-pagination]:items-center [&_.custom-pagination]:gap-3 
+  [&_.swiper-pagination-bullet]:!m-0 [&_.swiper-pagination-bullet]:!w-2 [&_.swiper-pagination-bullet]:!h-2 [&_.swiper-pagination-bullet]:!bg-[#40c4ff] [&_.swiper-pagination-bullet]:!opacity-[0.24] [&_.swiper-pagination-bullet]:rounded-full [&_.swiper-pagination-bullet]:transition-all [&_.swiper-pagination-bullet]:duration-300 
+  [&_.swiper-pagination-bullet-active]:!bg-[#40c4ff] [&_.swiper-pagination-bullet-active]:!opacity-100 [&_.swiper-pagination-bullet-active]:!w-2 [&_.swiper-pagination-bullet-active]:!h-2`;
 
 export default function HomeSlider() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const displaySliders = staticSliders;
+
+  if (!mounted) {
+    // Placeholder height to prevent layout shift
+    return <div className="overflow-hidden aspect-[2/1] sm:aspect-[21/9] lg:aspect-[10/3]" />;
+  }
+
   return (
-    <>
-    HomeSlider
-    </>
+    <div className="w-full">
+      {displaySliders.length > 0 && (
+        <div className={sliderWrapperStyles}>
+
+          {/* 👇 Dots wapas Top-Left par */}
+          <div className="custom-pagination"></div>
+
+          {/* 👇 Arrows wapas Top-Right par */}
+          <div className="absolute top-3 right-4 z-50 flex items-center gap-2">
+            <button className="custom-prev cursor-pointer text-white opacity-70 hover:opacity-100 transition-opacity p-1 rounded-full">
+              <svg className="w-5 h-5" focusable="false" aria-hidden="true" viewBox="0 0 24 24">
+                <path fill="currentColor" fillRule="evenodd" d="M15.488 4.43a.75.75 0 0 1 .081 1.058L9.988 12l5.581 6.512a.75.75 0 1 1-1.138.976l-6-7a.75.75 0 0 1 0-.976l6-7a.75.75 0 0 1 1.057-.081" clipRule="evenodd"></path>
+              </svg>
+            </button>
+
+            <button className="custom-next cursor-pointer text-white opacity-70 hover:opacity-100 transition-opacity p-1 rounded-full"> 
+              <svg className="w-5 h-5" focusable="false" aria-hidden="true" viewBox="0 0 24 24">
+                <path fill="currentColor" fillRule="evenodd" d="M8.512 4.43a.75.75 0 0 1 1.057.082l6 7a.75.75 0 0 1 0 .976l-6 7a.75.75 0 0 1-1.138-.976L14.012 12L8.431 5.488a.75.75 0 0 1 .08-1.057" clipRule="evenodd"></path>
+              </svg>
+            </button>
+          </div>
+
+          <SwiperComponent
+            modules={[Autoplay, Navigation, Pagination]}
+            slidesPerView={1}
+            loop
+            autoplay={{ delay: 5000, disableOnInteraction: false }}
+            autoHeight={false}
+            navigation={{
+              prevEl: ".custom-prev",
+              nextEl: ".custom-next",
+            }}
+            pagination={{
+              el: ".custom-pagination",
+              clickable: true
+            }}
+          >
+            {displaySliders.map((item, index) => (
+              <SwiperSlide key={item.id || index}>
+                {/* 👇 Height/Width properly responsive (AuExch jaisi) */}
+                <div className="w-full relative aspect-[2/1] sm:aspect-[21/9] lg:aspect-[10/3] max-h-[400px]">
+                  <Image
+                    src={item.url}
+                    alt={`Slider ${index + 1}`}
+                    fill
+                    className="w-full h-full object-fill md:object-cover"
+                    priority={index === 0}
+                    unoptimized
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </SwiperComponent>
+        </div>
+      )}
+    </div>
   );
 }
