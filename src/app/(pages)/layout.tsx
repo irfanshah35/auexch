23697246646cd832add679247bb8b7f35297a600
faizan99904 <faizan99904@gmail.com>
2026-02-15 18:11:58 +0500

    changes

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -183,7 +183,6 @@ export default function PagesLayout({ children }: { children: ReactNode }) {
         </div>
         <main className="pt-28 px-2 h-screen overflow-y-auto">
           {children}
-
           <Footer />
         </main>
       </div>
