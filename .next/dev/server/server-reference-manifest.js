a76fb3e55a8aa23d04cc39272dcc3565b9e4c9aa
faizan99904 <faizan99904@gmail.com>
2026-02-15 16:10:46 +0500

    chore: stop tracking .next build artifacts

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AGUrsmUAEW1LwL7/yAQzVApqaIQaPDqRmevqyla3Gg8=\"\n}"
--- .next/dev/static/chunks/Desktop_auexch_src_app_page_tsx_ea8cbb99._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/72895_71222b9a._.js",
-  "static/chunks/Desktop_auexch_src_431b28aa._.js",
-  "static/chunks/Desktop_auexch_9e78e7dc._.css"
-],
-    source: "dynamic"
-});
