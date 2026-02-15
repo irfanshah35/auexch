9e9d374ffaf9a63761218700d38b29d53f917096
irfanshah35 <irfanshah@universalplus.technology>
2026-02-15 12:37:25 +0500

    changes

--- .next/dev/static/chunks/Desktop_auexch_src_app_page_tsx_ea8cbb99._.js
+++ .next/dev/static/chunks/Desktop_auexch_src_app_page_tsx_ea8cbb99._.js
@@ -2,8 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/72895_71222b9a._.js",
-  "static/chunks/Desktop_auexch_src_961fd94e._.js",
-  "static/chunks/72895_swiper_d930a848._.css"
+  "static/chunks/Desktop_auexch_src_431b28aa._.js",
+  "static/chunks/Desktop_auexch_9e78e7dc._.css"
 ],
     source: "dynamic"
 });
--- src/components/sports-nave/index.tsx
+++ src/components/sports-nave/index.tsx
@@ -1,11 +1,48 @@
 "use client";
 
- 
+import { useState } from "react";
+import styles from "./sportsPage.module.css";
+
+const tabs = ["Cricket", "Soccer", "Tennis"];
 
 export default function SportsNave() {
+  const [activeTab, setActiveTab] = useState("Cricket");
+
   return (
-    <>
-    SportsNave
-    </>
+    <section>
+      <h2 className={styles["sr-only"]}>Explore Cricket Betting</h2>
+
+      <div className={styles["tabs-root"]}>
+        <div className="MuiButtonBase-root MuiTabScrollButton-root MuiTabScrollButton-horizontal Mui-disabled MuiTabs-scrollButtons css-xuc7kg"><svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall css-qgieeu" focusable="false" aria-hidden="true" viewBox="0 0 24 24"><path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"></path></svg><span className="MuiTouchRipple-root css-4mb1j7"></span></div>
+        <div className={styles["tabs-scroller"]}>
+          <div role="tablist" className={styles["tabs-list"]}>
+            {tabs.map((tab) => (
+              <button
+                key={tab}
+                role="tab"
+                aria-selected={activeTab === tab}
+                className={`${styles["tab-btn"]} ${activeTab === tab ? styles.active : ""
+                  }`}
+                onClick={() => setActiveTab(tab)}
+              >
+                {tab}
+                <span
+                  className={`${styles["tab-icon"]} ${styles[`icon-${tab.toLowerCase().replace(/\s/g, "-")}`]
+                    }`}
+                />
+              </button>
+            ))}
+          </div>
+
+          {/* Indicator */}
+          <span
+            className={styles["tab-indicator"]}
+            style={{
+              /* optional: move indicator dynamically later */
+            }}
+          />
+        </div>
+      </div>
+    </section>
   );
 }
